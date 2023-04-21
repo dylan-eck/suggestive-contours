3d449e1e866f3f64d9def9029e45f0fe40b94bd2
Dylan Eck <akdylaneck@gmail.com>
2023-04-21 17:38:38 -0600

    implement post processing render pass

--- src/main.cpp
+++ src/main.cpp
@@ -362,17 +362,36 @@ int main(void)
 
     glBindBuffer(GL_UNIFORM_BUFFER, 0);
 
-    sphere.modelMatrix =  glm::scale(
-    glm::translate(
-        glm::mat4(1.0f),
-        lightPosition),
-        glm::vec3(0.1f)
-    );
+    GLuint framebuffer;
+    glGenFramebuffers(1, &framebuffer);
+    glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
+
+    GLuint textureColorbuffer;
+    glGenTextures(1, &textureColorbuffer);
+    glBindTexture(GL_TEXTURE_2D, textureColorbuffer);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR );
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glBindTexture(GL_TEXTURE_2D, 0);
+
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, textureColorbuffer, 0);
+
+    GLuint rbo;
+    glGenRenderbuffers(1, &rbo);
+    glBindRenderbuffer(GL_RENDERBUFFER, rbo);
+    glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, width, height);
+    glBindRenderbuffer(GL_RENDERBUFFER, 0);
+
+    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, rbo);
+
+    if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE) {
+        printf("error: post processing frame buffer incomplete.\n");
+    }
+
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
 
     // set up renderer
     Renderer renderer;
-    glEnable(GL_DEPTH_TEST);
-    glClearColor(1.0, 1.0, 1.0, 1.0);
 
     // main rendering loop
     while (!glfwWindowShouldClose(window)) {
@@ -435,13 +454,23 @@ int main(void)
         glBindBuffer(GL_UNIFORM_BUFFER, 0);
 
         glViewport(0, 0, width, height);
+
+        glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
+        glClearColor(0.1f, 0.1f, 0.1f, 0.1f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glEnable(GL_DEPTH_TEST);
 
         renderer.setShaderProgram(modelProg);
         renderer.drawModel(model);
 
+        // post processing
+        glBindFramebuffer(GL_FRAMEBUFFER, 0);
+        glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
         glUseProgram(postProcessingProg);
         glBindVertexArray(-1);
+        glDisable(GL_DEPTH_TEST);
+        glBindTexture(GL_TEXTURE_2D, textureColorbuffer);
         glDrawArrays(GL_TRIANGLES, 0, 3);
 
         glfwSwapBuffers(window);
--- src/shaders/screen_quad.frag.glsl
+++ src/shaders/screen_quad.frag.glsl
@@ -4,6 +4,9 @@ in vec2 texcoords;
 
 out vec4 color;
 
+uniform sampler2D screenTexture;
+
 void main() {
-    color = vec4(texcoords, 0.0, 1.0);
+    // color = vec4(floor(texcoords), 0.0, 1.0);
+    color = texture(screenTexture, texcoords) * vec4(1.0, 0.0, 0.0, 1.0);
 }
